56df84a0a715f10b6aa87c9f645168022b63f7b9
Hsync <Hsync@HP-OMEN-16>
2024-04-14 00:07:57 +0500

    Add EF, Connection Strings

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using rpgapi.Models;
+
+namespace rpgapi.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
